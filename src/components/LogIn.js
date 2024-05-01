a86a8531c8c78d73049b87006b8dfdc41886e704
aditya <mp7020216@gmail.com>
2024-05-02 00:11:25 +0530

    Update Login Page a bit

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,6 +1,13 @@
 import Header from "./Header";
+import { useState } from "react";
 
 const LogIn = () => {
+
+  const [isSignedIn, setIsSignedIn] = useState(true);
+  const toggleSignIn = () => {
+    setIsSignedIn(!isSignedIn);
+  };
+
   return (
     <div>
         <Header />
@@ -11,7 +18,14 @@ const LogIn = () => {
             />
         </div>
         <form className="w-3/12 absolute p-12 bg-black bg-opacity-80 my-36 mx-auto text-white right-0 left-0 rounded-lg">
-            <h1 className="font-bold text-3xl py-4">Sign In</h1>
+            <h1 className="font-bold text-3xl py-4">
+                {isSignedIn ? "Sign In" : "Sign Up"}
+            </h1>
+            {!isSignedIn && <input 
+                type="text" 
+                placeholder="Name" 
+                className="p-4 my-4 w-full bg-gray-800 rounded-lg" 
+            />}
             <input 
                 type="text" 
                 placeholder="Email Address" 
@@ -22,9 +36,26 @@ const LogIn = () => {
                 placeholder="Password" 
                 className="p-4 my-4 w-full bg-gray-800 rounded-lg" 
             />
-            <button className="p-4 my-6 bg-red-600 w-full rounded-lg" >
-                Sign In
+            <button className="p-4 my-6 bg-red-600 w-full rounded-lg">
+                {isSignedIn ? "Sign In" : "Sign Up"}
             </button>
+            <p className="py-6 cursor-pointer" onClick={toggleSignIn}>
+                {!isSignedIn ? (
+                    <span>
+                        Already a User?{" "}
+                        <span className="hover:underline hover:text-gray-700" onClick={toggleSignIn}>
+                            Sign In Now
+                        </span>
+                    </span>
+                    ) : (
+                    <span>
+                        New to Netflix?{" "}
+                        <span className="hover:underline hover:text-gray-500" onClick={toggleSignIn}>
+                            Sign Up Now
+                        </span>
+                    </span>
+                )} 
+            </p>
         </form>
     </div>
   )};
